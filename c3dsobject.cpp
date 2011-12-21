f1137c32edbe0da7e29103b00061ee3b33fbb192
Amund Hov <amundhov@samfundet.no>
2011-12-21 18:11:21 +0100

    Use Log, kukfaen

--- c3dsobject.cpp
+++ c3dsobject.cpp
@@ -114,7 +114,7 @@ void C3dsScene::parseFile(string filename)
                 break;
 
             default:
-                printf("Unkown chunk %04x of length %u\n", parser.getChunkId(), parser.getChunkLength());
+                Log("Unkown chunk %04x of length %u\n", parser.getChunkId(), parser.getChunkLength());
                 parser.skipChunk();
                 break;
 
